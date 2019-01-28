766b18e28e71a63380d76fdf02ae65aca59e4c06
Masashi Sekine <m_sekine@tokyootakumode.com>
2019-01-28 09:53:22 +0900

    Updated to use GeoLite2 data created by MaxMind and updated to change the data format to reduce data volume

---
+++ README.md
@@ -0,0 +1,16 @@
+# Country By IP
+
+You can retrieve country name (English / Japanese) and country code by IP address.
+
+
+## Running the tests
+
+```
+npm test
+
+```
+
+## License
+
+This project is licensed under the MIT License and includes GeoLite2 data created by MaxMind, available from
+[https://www.maxmind.com](https://www.maxmind.com).
---
+++ data/countries.js
@@ -0,0 +1,337 @@
+'use strict';
+
+module.exports = {
+  AC: { name: { en: 'Ascension Island', ja: 'アセンション島' } },
+  AD: { name: { en: 'Andorra', ja: 'アンドラ' } },
+  AE: { name: { en: 'United Arab Emirates', ja: 'アラブ首長国連邦' } },
+  AF: { name: { en: 'Afghanistan', ja: 'アフガニスタン' } },
+  AG: { name: { en: 'Antigua And Barbuda', ja: 'アンティグァ＝バーブーダ' } },
+  AI: { name: { en: 'Anguilla', ja: 'アンギーラ島(イギリス領)' } },
+  AL: { name: { en: 'Albania', ja: 'アルバニア' } },
+  AM: { name: { en: 'Armenia', ja: 'アルメニア' } },
+  AN: { name: { en: 'Netherlands Antilles', ja: 'オランダ領アンティル' } },
+  AO: { name: { en: 'Angola', ja: 'アンゴラ' } },
+  AP: { name: { en: 'Asia Pacific', ja: 'アジア太平洋連合' } },
+  AQ: { name: { en: 'Antarctica', ja: '南極' } },
+  AR: { name: { en: 'Argentina', ja: 'アルゼンチン' } },
+  AS: { name: { en: 'American Samoa', ja: 'アメリカンサモア' } },
+  AT: { name: { en: 'Austria', ja: 'オーストリア' } },
+  AU: { name: { en: 'Australia', ja: 'オーストラリア' } },
+  AW: { name: { en: 'Aruba', ja: 'アルバ(オランダ自治領)' } },
+  AX: { name: { en: 'Åland', ja: 'オーランド諸島' } },
+  AZ: { name: { en: 'Azerbaijan', ja: 'アゼルバイジャン' } },
+  BA: {
+    name: { en: 'Bosnia And Herzegowina', ja: 'ボスニア＝ヘルツェゴビナ' }
+  },
+  BB: { name: { en: 'Barbados', ja: 'バルバドス' } },
+  BD: { name: { en: 'Bangladesh', ja: 'バングラデシュ' } },
+  BE: { name: { en: 'Belgium', ja: 'ベルギー' } },
+  BF: { name: { en: 'Burkina Faso', ja: 'ブルキナファソ' } },
+  BG: { name: { en: 'Bulgaria', ja: 'ブルガリア' } },
+  BH: { name: { en: 'Bahrain', ja: 'バハレーン' } },
+  BI: { name: { en: 'Burundi', ja: 'ブルンジ' } },
+  BJ: { name: { en: 'Benin', ja: 'ベナン' } },
+  BL: { name: { en: 'Saint Barthélemy', ja: 'サン・バルテルミー' } },
+  BM: { name: { en: 'Bermuda', ja: 'バーミューダ諸島(イギリス領)' } },
+  BN: { name: { en: 'Brunei Darussalam', ja: 'ブルネイ' } },
+  BO: { name: { en: 'Bolivia', ja: 'ボリビア' } },
+  BQ: {
+    name: {
+      en: 'Bonaire, Sint Eustatius, and Saba',
+      ja: 'ボネール、シント・ユースタティウスおよびサバ'
+    }
+  },
+  BR: { name: { en: 'Brazil', ja: 'ブラジル' } },
+  BS: { name: { en: 'Bahamas', ja: 'バハマ' } },
+  BT: { name: { en: 'Bhutan', ja: 'ブータン' } },
+  BV: { name: { en: 'Bouvet Island', ja: 'ブーベ島(ノルウェー領)' } },
+  BW: { name: { en: 'Botswana', ja: 'ボツワナ' } },
+  BY: { name: { en: 'Belarus', ja: 'ベラルーシ' } },
+  BZ: { name: { en: 'Belize', ja: 'ベリーズ' } },
+  CA: { name: { en: 'Canada', ja: 'カナダ' } },
+  CC: {
+    name: { en: 'Cocos (Keeling) Islands', ja: 'ココス諸島(オーストラリア領)' }
+  },
+  CD: {
+    name: { en: 'Congo', ja: 'コンゴ民主共和国' }
+  },
+  CF: { name: { en: 'Central African Republic', ja: '中央アフリカ共和国' } },
+  CG: { name: { en: 'Congo', ja: 'コンゴ' } },
+  CH: { name: { en: 'Switzerland', ja: 'スイス' } },
+  CI: {
+    name: {
+      en: "Cote D'Ivoire(Ivory Coast)",
+      ja: 'コートジボアール（象牙海岸）'
+    }
+  },
+  CK: {
+    name: { en: 'Cook Islands', ja: 'クック諸島(ニュージーランド自治領)' }
+  },
+  CL: { name: { en: 'Chile', ja: 'チリ' } },
+  CM: { name: { en: 'Cameroon', ja: 'カメルーン' } },
+  CN: { name: { en: 'China', ja: '中国' } },
+  CO: { name: { en: 'Colombia', ja: 'コロンビア' } },
+  CR: { name: { en: 'Costa Rica', ja: 'コスタリカ' } },
+  CS: { name: { en: 'Czechoslovakia (former)', ja: '旧チェコスロバキア' } },
+  CU: { name: { en: 'Cuba', ja: 'キューバ' } },
+  CV: { name: { en: 'Cape Verde', ja: 'カーボベルデ' } },
+  CW: { name: { en: 'Curaçao', ja: 'キュラソー' } },
+  CX: { name: { en: 'Christmas Island', ja: 'クリスマス諸島' } },
+  CY: { name: { en: 'Cyprus', ja: 'キプロス' } },
+  CZ: { name: { en: 'Czech Republic', ja: 'チェコ共和国' } },
+  DE: { name: { en: 'Germany', ja: 'ドイツ' } },
+  DJ: { name: { en: 'Djibouti', ja: 'ジブチ' } },
+  DK: { name: { en: 'Denmark', ja: 'デンマーク' } },
+  DM: { name: { en: 'Dominica', ja: 'ドミニカ連邦' } },
+  DO: { name: { en: 'Dominican Republic', ja: 'ドミニカ共和国' } },
+  DZ: { name: { en: 'Algeria', ja: 'アルジェリア' } },
+  EC: { name: { en: 'Ecuador', ja: 'エクアドル' } },
+  EE: { name: { en: 'Estonia', ja: 'エストニア' } },
+  EG: { name: { en: 'Egypt', ja: 'エジプト' } },
+  EH: { name: { en: 'Western Sahara', ja: '西サハラ' } },
+  ER: { name: { en: 'Eritrea', ja: 'エリトリア' } },
+  ES: { name: { en: 'Spain', ja: 'スペイン' } },
+  ET: { name: { en: 'Ethiopia', ja: 'エチオピア' } },
+  EU: { name: { en: 'EU', ja: '欧州連合' } },
+  FI: { name: { en: 'Finland', ja: 'フィンランド' } },
+  FJ: { name: { en: 'Fiji', ja: 'フィジー' } },
+  FK: {
+    name: {
+      en: 'Falkland Islands (Malvinas)',
+      ja: 'フォークランド諸島(イギリス領)'
+    }
+  },
+  FM: {
+    name: { en: 'Micronesia, Federated States Of', ja: 'ミクロネシア連邦' }
+  },
+  FO: { name: { en: 'Faroe Islands', ja: 'フェロー諸島(デンマーク領)' } },
+  FR: { name: { en: 'France', ja: 'フランス' } },
+  FX: { name: { en: 'France, Metropolitan', ja: 'フランス(首都圏？)' } },
+  GA: { name: { en: 'Gabon', ja: 'ガボン' } },
+  GB: { name: { en: 'Great Britain(United Kingdom)', ja: 'イギリス' } },
+  GD: { name: { en: 'Grenada', ja: 'グレナダ' } },
+  GE: { name: { en: 'Georgia', ja: 'グルジア' } },
+  GF: { name: { en: 'French Guiana', ja: 'フランス領ギアナ' } },
+  GG: { name: { en: 'Guernsey', ja: 'ガーンジー' } },
+  GH: { name: { en: 'Ghana', ja: 'ガーナ' } },
+  GI: { name: { en: 'Gibraltar', ja: 'ジブラルタル(イギリス領)' } },
+  GL: { name: { en: 'Greenland', ja: 'グリーンランド' } },
+  GM: { name: { en: 'Gambia', ja: 'ガンビア' } },
+  GN: { name: { en: 'Guinea', ja: 'ギニア' } },
+  GP: { name: { en: 'Guadeloupe', ja: 'グアドループ(フランス領)' } },
+  GQ: { name: { en: 'Equatorial Guinea', ja: '赤道ギニア' } },
+  GR: { name: { en: 'Greece', ja: 'ギリシャ' } },
+  GS: {
+    name: {
+      en: 'South Georgia And The South Sandwich Islands',
+      ja: 'サウスジョージア島・サウスサンドイッチ島(イギリス領)'
+    }
+  },
+  GT: { name: { en: 'Guatemala', ja: 'グアテマラ' } },
+  GU: { name: { en: 'Guam', ja: 'グアム(米国領)' } },
+  GW: { name: { en: 'Guinea-Bissau', ja: 'ギニアビサオ' } },
+  GY: { name: { en: 'Guyana', ja: 'ガイアナ' } },
+  HK: { name: { en: 'Hong Kong', ja: '香港' } },
+  HM: {
+    name: { en: 'Heard And Mc Donald Islands', ja: 'ハード・マクドナルド諸島' }
+  },
+  HN: { name: { en: 'Honduras', ja: 'ホンジュラス' } },
+  HR: { name: { en: 'Croatia (Hrvatska)', ja: 'クロアチア' } },
+  HT: { name: { en: 'Haiti', ja: 'ハイチ' } },
+  HU: { name: { en: 'Hungary', ja: 'ハンガリー' } },
+  ID: { name: { en: 'Indonesia', ja: 'インドネシア' } },
+  IE: { name: { en: 'Ireland', ja: 'アイルランド' } },
+  IL: { name: { en: 'Israel', ja: 'イスラエル' } },
+  IM: { name: { en: 'Isle of Man', ja: 'マン島' } },
+  IN: { name: { en: 'India', ja: 'インド' } },
+  IO: {
+    name: { en: 'British Indian Ocean Territory', ja: 'イギリスインド洋領' }
+  },
+  IQ: { name: { en: 'Iraq', ja: 'イラク' } },
+  IR: { name: { en: 'Iran (Islamic Republic Of)', ja: 'イラン' } },
+  IS: { name: { en: 'Iceland', ja: 'アイスランド' } },
+  IT: { name: { en: 'Italy', ja: 'イタリア' } },
+  JE: { name: { en: 'Jersey', ja: 'ジャージー' } },
+  JM: { name: { en: 'Jamaica', ja: 'ジャマイカ' } },
+  JO: { name: { en: 'Jordan', ja: 'ヨルダン' } },
+  JP: { name: { en: 'Japan', ja: '日本' } },
+  KE: { name: { en: 'Kenya', ja: 'ケニア' } },
+  KG: { name: { en: 'Kyrgyzstan', ja: 'キルギスタン' } },
+  KH: { name: { en: 'Cambodia', ja: 'カンボジア' } },
+  KI: { name: { en: 'Kiribati', ja: 'キリバス' } },
+  KM: { name: { en: 'Comoros', ja: 'コモロ' } },
+  KN: {
+    name: { en: 'Saint Kitts And Nevis', ja: 'セントクリストファー・ネイビス' }
+  },
+  KP: { name: { en: 'Korea(North)', ja: '北朝鮮' } },
+  KR: { name: { en: 'Korea, Republic Of', ja: '韓国' } },
+  KW: { name: { en: 'Kuwait', ja: 'クウェート' } },
+  KY: { name: { en: 'Cayman Islands', ja: 'ケイマン諸島(イギリス領)' } },
+  KZ: { name: { en: 'Kazakhstan', ja: 'カザフスタン' } },
+  LA: { name: { en: 'Laos', ja: 'ラオス' } },
+  LB: { name: { en: 'Lebanon', ja: 'レバノン' } },
+  LC: { name: { en: 'Saint Lucia', ja: 'セントルシア' } },
+  LI: { name: { en: 'Liechtenstein', ja: 'リヒテンシュタイン' } },
+  LK: { name: { en: 'Sri Lanka', ja: 'スリランカ' } },
+  LR: { name: { en: 'Liberia', ja: 'リベリア' } },
+  LS: { name: { en: 'Lesotho', ja: 'レソト' } },
+  LT: { name: { en: 'Lithuania', ja: 'リトアニア' } },
+  LU: { name: { en: 'Luxembourg', ja: 'ルクセンブルク' } },
+  LV: { name: { en: 'Latvia', ja: 'ラトビア' } },
+  LY: { name: { en: 'Libyan Arab Jamahiriya', ja: 'リビア' } },
+  MA: { name: { en: 'Morocco', ja: 'モロッコ' } },
+  MC: { name: { en: 'Monaco', ja: 'モナコ' } },
+  MD: { name: { en: 'Moldova, Republic Of', ja: 'モルドバ' } },
+  ME: { name: { en: 'Montenegro', ja: 'モンテネグロ' } },
+  MF: { name: { en: 'Saint Martin', ja: 'サン・マルタン(フランス領)' } },
+  MG: { name: { en: 'Madagascar', ja: 'マダガスカル' } },
+  MH: { name: { en: 'Marshall Islands', ja: 'マーシャル諸島(米自由連合)' } },
+  MK: {
+    name: { en: 'Macedonia, The Former Yugoslav Republic Of', ja: 'マケドニア' }
+  },
+  ML: { name: { en: 'Mali', ja: 'マリ' } },
+  MM: { name: { en: 'Myanmar', ja: 'ミャンマー' } },
+  MN: { name: { en: 'Mongolia', ja: 'モンゴル' } },
+  MO: { name: { en: 'Macau', ja: 'マカオ' } },
+  MP: { name: { en: 'Northern Mariana Islands', ja: '北マリアナ諸島' } },
+  MQ: { name: { en: 'Martinique', ja: 'マルチニーク(フランス領)' } },
+  MR: { name: { en: 'Mauritania', ja: 'モーリタニア' } },
+  MS: { name: { en: 'Montserrat', ja: 'モントセラト(イギリス領)' } },
+  MT: { name: { en: 'Malta', ja: 'マルタ' } },
+  MU: { name: { en: 'Mauritius', ja: 'モーリシャス' } },
+  MV: { name: { en: 'Maldives', ja: 'モルディブ' } },
+  MW: { name: { en: 'Malawi', ja: 'マラウイ' } },
+  MX: { name: { en: 'Mexico', ja: 'メキシコ' } },
+  MY: { name: { en: 'Malaysia', ja: 'マレーシア' } },
+  MZ: { name: { en: 'Mozambique', ja: 'モザンビーク' } },
+  NA: { name: { en: 'Namibia', ja: 'ナミビア' } },
+  NC: { name: { en: 'New Caledonia', ja: 'ニューカレドニア(フランス領)' } },
+  NE: { name: { en: 'Niger', ja: 'ニジェール' } },
+  NF: {
+    name: { en: 'Norfolk Island', ja: 'ノーフォーク諸島(オーストラリア領)' }
+  },
+  NG: { name: { en: 'Nigeria', ja: 'ナイジェリア' } },
+  NI: { name: { en: 'Nicaragua', ja: 'ニカラグア' } },
+  NL: { name: { en: 'Netherlands', ja: 'オランダ' } },
+  NO: { name: { en: 'Norway', ja: 'ノルウェー' } },
+  NP: { name: { en: 'Nepal', ja: 'ネパール' } },
+  NR: { name: { en: 'Nauru', ja: 'ナウル' } },
+  NT: { name: { en: 'Neutral Zone', ja: '中立地帯' } },
+  NU: { name: { en: 'Niue', ja: 'ニウエ(ニュージーランド自治領)' } },
+  NZ: { name: { en: 'New Zealand', ja: 'ニュージーランド' } },
+  OM: { name: { en: 'Oman', ja: 'オマーン' } },
+  PA: { name: { en: 'Panama', ja: 'パナマ' } },
+  PE: { name: { en: 'Peru', ja: 'ペルー' } },
+  PF: { name: { en: 'French Polynesia', ja: 'フランス領ポリネシア' } },
+  PG: { name: { en: 'Papua New Guinea', ja: 'パプアニューギニア' } },
+  PH: { name: { en: 'Philippines', ja: 'フィリピン' } },
+  PK: { name: { en: 'Pakistan', ja: 'パキスタン' } },
+  PL: { name: { en: 'Poland', ja: 'ポーランド' } },
+  PM: {
+    name: {
+      en: 'St. Pierre And Miquelon',
+      ja: 'サンピエール・ミクロン諸島(フランス領)'
+    }
+  },
+  PN: { name: { en: 'Pitcairn', ja: 'ピトケアン諸島(イギリス領)' } },
+  PR: { name: { en: 'Puerto Rico', ja: 'プエルトリコ' } },
+  PS: { name: { en: 'Palestina', ja: 'パレスチナ' } },
+  PT: { name: { en: 'Portugal', ja: 'ポルトガル' } },
+  PW: { name: { en: 'Palau', ja: 'パラオ' } },
+  PY: { name: { en: 'Paraguay', ja: 'パラグアイ' } },
+  QA: { name: { en: 'Qatar', ja: 'カタール' } },
+  RE: { name: { en: 'Reunion', ja: 'レユニオン島(フランス領)' } },
+  RO: { name: { en: 'Romania', ja: 'ルーマニア' } },
+  RS: { name: { en: 'Serbia', ja: 'セルビア' } },
+  RU: { name: { en: 'Russian Federation', ja: 'ロシア連邦' } },
+  RW: { name: { en: 'Rwanda', ja: 'ルワンダ' } },
+  SA: { name: { en: 'Saudi Arabia', ja: 'サウジアラビア' } },
+  SB: { name: { en: 'Solomon Islands', ja: 'ソロモン諸島' } },
+  SC: { name: { en: 'Seychelles', ja: 'セイシェル' } },
+  SD: { name: { en: 'Sudan', ja: 'スーダン' } },
+  SE: { name: { en: 'Sweden', ja: 'スウェーデン' } },
+  SG: { name: { en: 'Singapore', ja: 'シンガポール' } },
+  SH: { name: { en: 'St. Helena', ja: 'セントヘレナ島(イギリス領)' } },
+  SI: { name: { en: 'Slovenia', ja: 'スロベニア' } },
+  SJ: {
+    name: {
+      en: 'Svalbard And Jan Mayen Islands',
+      ja: 'スバールバル・ヤンマイエン島(ノルウェー領)'
+    }
+  },
+  SK: { name: { en: 'Slovakia (Slovak Republic)', ja: 'スロバキア' } },
+  SL: { name: { en: 'Sierra Leone', ja: 'シエラレオネ' } },
+  SM: { name: { en: 'San Marino', ja: 'サンマリノ' } },
+  SN: { name: { en: 'Senegal', ja: 'セネガル' } },
+  SO: { name: { en: 'Somalia', ja: 'ソマリア' } },
+  SR: { name: { en: 'Suriname', ja: 'スリナム' } },
+  SS: { name: { en: 'South Sudan', ja: '南スーダン' } },
+  ST: { name: { en: 'Sao Tome And Principe', ja: 'サントメ・プリンシペ' } },
+  SU: { name: { en: 'USSR (former)', ja: '旧ソビエト連邦' } },
+  SV: { name: { en: 'El Salvador', ja: 'エルサルバドル' } },
+  SX: { name: { en: 'Sint Maarten', ja: 'シント・マールテン' } },
+  SY: { name: { en: 'Syrian Arab Republic', ja: 'シリア' } },
+  SZ: { name: { en: 'Swaziland', ja: 'スワジランド' } },
+  TC: {
+    name: {
+      en: 'Turks And Caicos Islands',
+      ja: 'タークス・ナイコス諸島(イギリス領)'
+    }
+  },
+  TD: { name: { en: 'Chad', ja: 'チャド' } },
+  TF: { name: { en: 'French Southern Territories', ja: 'フランス南方領土' } },
+  TG: { name: { en: 'Togo', ja: 'トーゴ' } },
+  TH: { name: { en: 'Thailand', ja: 'タイ' } },
+  TJ: { name: { en: 'Tajikistan', ja: 'タジキスタン' } },
+  TK: { name: { en: 'Tokelau', ja: 'トケラウ(ニュージーランド自治領)' } },
+  TL: {
+    name: { en: 'Democratic Republic of Timor-Leste', ja: '東ティモール共和国' }
+  },
+  TM: { name: { en: 'Turkmenistan', ja: 'トルクメニスタン' } },
+  TN: { name: { en: 'Tunisia', ja: 'チュニジア' } },
+  TO: { name: { en: 'Tonga', ja: 'トンガ' } },
+  TP: { name: { en: 'East Timor', ja: '東ティモール（インドネシア領）' } },
+  TR: { name: { en: 'Turkey', ja: 'トルコ' } },
+  TT: { name: { en: 'Trinidad And Tobago', ja: 'トリニダード・トバゴ' } },
+  TV: { name: { en: 'Tuvalu', ja: 'ツバル' } },
+  TW: { name: { en: 'Taiwan, Province Of China', ja: '台湾' } },
+  TZ: { name: { en: 'Tanzania, United Republic Of', ja: 'タンザニア' } },
+  UA: { name: { en: 'Ukraine', ja: 'ウクライナ' } },
+  UG: { name: { en: 'Uganda', ja: 'ウガンダ' } },
+  UK: { name: { en: 'United Kingdom', ja: 'イギリス' } },
+  UM: {
+    name: { en: 'United States Minor Outlying Islands', ja: '合衆国周辺離島' }
+  },
+  US: { name: { en: 'United States', ja: 'アメリカ' } },
+  UY: { name: { en: 'Uruguay', ja: 'ウルグアイ' } },
+  UZ: { name: { en: 'Uzbekistan', ja: 'ウズベキスタン' } },
+  VA: { name: { en: 'Vatican City State (Holy See)', ja: 'バチカン市国' } },
+  VC: {
+    name: {
+      en: 'Saint Vincent And The Grenadines',
+      ja: 'セントビンセントおよびグレナディン諸島'
+    }
+  },
+  VE: { name: { en: 'Venezuela', ja: 'ベネズエラ' } },
+  VG: {
+    name: { en: 'Virgin Islands (British)', ja: 'バージン諸島(イギリス領)' }
+  },
+  VI: { name: { en: 'Virgin Islands (U.S.)', ja: 'バージン諸島(米国領)' } },
+  VN: { name: { en: 'Viet Nam', ja: 'ベトナム' } },
+  VU: { name: { en: 'Vanuatu', ja: 'バヌアツ' } },
+  WF: {
+    name: {
+      en: 'Wallis And Futuna Islands',
+      ja: 'ワリスフツナ諸島(フランス領)'
+    }
+  },
+  WS: { name: { en: 'Samoa', ja: '西サモア' } },
+  XK: { name: { en: 'Kosovo', ja: 'コソボ' } },
+  YE: { name: { en: 'Yemen', ja: 'イエメン' } },
+  YT: { name: { en: 'Mayotte', ja: 'マヨット島(フランス領)' } },
+  YU: { name: { en: 'Yugoslavia', ja: 'ユーゴスラビア' } },
+  ZA: { name: { en: 'South Africa', ja: '南アフリカ' } },
+  ZM: { name: { en: 'Zambia', ja: 'ザンビア' } },
+  ZR: { name: { en: 'Zaire', ja: 'ザイール' } },
+  ZW: { name: { en: 'Zimbabwe', ja: 'ジンバブエ' } }
+};
